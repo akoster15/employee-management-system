9b9efa8050d549e2dd8b62a08ff7b21da29fd57e
Andrew Koster <andrewkoster95@gmail.com>
2021-07-19 21:49:17 -0400

    created server.js and added node modules

---
+++ server.js
@@ -0,0 +1,9 @@
+//Import modules
+const express = require('express');
+const path = require("path");
+const mongoose = require('mongoose');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
